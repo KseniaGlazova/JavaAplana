d7da8593a634dc22dfecb44d733848fbf5d2aa24
KseniaGlazova <ksenia.glazova@gmail.com>
2020-02-13 18:20:53 +0300

    try

--- src/Again.java
+++ src/Again.java
@@ -1,7 +1,7 @@
 public class Again {
             public static void main(String[] args)
         {
-            System.out.println("Hello, world!");
+            System.out.println("Hello, world!!!");
         }
     }
 
