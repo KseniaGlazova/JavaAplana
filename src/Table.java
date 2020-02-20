c245e7dce9387880c28b050e70765b1e996637de
KseniaGlazova <ksenia.glazova@gmail.com>
2020-02-20 18:53:42 +0300

    Таблица умножения

---
+++ src/Table.java
@@ -0,0 +1,34 @@
+public class Table {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println(x + " " + x * 2 + " " + x * 3 + " " + x * 4 + " " + x * 5 + " " + x * 6 + " " + x * 7
+                + " " + x * 8 + " " + x * 9 + " " + x * 10);
+        int a = 2;
+        System.out.println(a + " " + a * 2 + " " + a * 3 + " " + a * 4 + " " + a * 5 + " " + a * 6 + " " + a * 7
+                + " " + a * 8 + " " + a * 9 + " " + a * 10);
+        int b = 3;
+        System.out.println(b + " " + b * 2 + " " + b * 3 + " " + b * 4 + " " + b * 5 + " " + b * 6 + " " + b * 7
+                + " " + b * 8 + " " + b * 9 + " " + b * 10);
+        int c = 4;
+        System.out.println(c + " " + c * 2 + " " + c * 3 + " " + c * 4 + " " + c * 5 + " " + c * 6 + " " + c * 7
+                + " " + c * 8 + " " + c * 9 + " " + c * 10);
+        int d = 5;
+        System.out.println(d + " " + d * 2 + " " + d * 3 + " " + d * 4 + " " + d * 5 + " " + d * 6 + " " + d * 7
+                + " " + d * 8 + " " + d * 9 + " " + d * 10);
+        int e = 6;
+        System.out.println(e + " " + e * 2 + " " + e * 3 + " " + e * 4 + " " + e * 5 + " " + e * 6 + " " + e * 7
+                + " " + e * 8 + " " + e * 9 + " " + e * 10);
+        int f= 7;
+        System.out.println(f + " " + f * 2 + " " + f * 3 + " " + f * 4 + " " + f * 5 + " " + f * 6 + " " + f * 7
+                + " " + f * 8 + " " + f * 9 + " " + f * 10);
+        int g = 8;
+        System.out.println(g + " " + g * 2 + " " + g * 3 + " " + g * 4 + " " + g * 5 + " " + g * 6 + " " + g * 7
+                + " " + g * 8 + " " + g * 9 + " " + g * 10);
+        int h = 9;
+        System.out.println(h + " " + h * 2 + " " + h * 3 + " " + h * 4 + " " + h * 5 + " " + h * 6 + " " + h * 7
+                + " " + h * 8 + " " + h * 9 + " " + h * 10);
+        int l = 10;
+        System.out.println(l + " " + l * 2 + " " + l * 3 + " " + l * 4 + " " + l * 5 + " " + l * 6 + " " + l * 7
+                + " " + l * 8 + " " + l * 9 + " " + l * 10);
+    }
+}
