193f848ccd1a28995004f6cd401b61cf538c6464
KseniaGlazova <ksenia.glazova@gmail.com>
2020-02-22 16:36:45 +0300

    Задание 7

--- src/Constanta.java
+++ src/Constanta.java
@@ -1,7 +1,22 @@
-/*import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Constanta {
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-}*/
+        int x = 4;
+        int y = 5;
+        int z = 6;
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number == x) {
+            System.out.println("Данное значение есть в константах");
+        } else if (number == y) {
+            System.out.println("Данное значение есть в константах");
+        } else if (number == z){
+            System.out.println("Данное значение есть в константах");
+        }
+        else {
+            System.out.println("Такой константы нет!");
+        }
+    }
+}
