a0493cff00957ecec4dc59a942d855864525098b
KseniaGlazova <ksenia.glazova@gmail.com>
2020-02-23 14:16:29 +0300

    Поправленное задание 9

--- src/MassiveFirst.java
+++ src/MassiveFirst.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class MassiveFirst {
     public static void main(String[] args) {
-        int[] numbers = {2, 4, 6, 8, 10, 12};
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[5];
+        int i = 0;
+
+        for (int element : numbers){
+            numbers[i] = scanner.nextInt();
+            i++;
+        }
+
         for (int x : numbers){
             x = x * 2;
             System.out.println(x);
