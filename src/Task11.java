2d48705a821fa1b3aa33bb40796683d673129d94
KseniaGlazova <ksenia.glazova@gmail.com>
2020-02-24 11:30:30 +0300

    Задание 11

---
+++ src/Task11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Task11 {
+    public static void main (String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Напиши что-нибудь классное: ");
+        String klass = sc.nextLine();
+        System.out.println(klass);
+    }
+}
+
