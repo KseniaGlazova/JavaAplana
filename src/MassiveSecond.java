441c9c7c8643a9d7473881b006fd65d2f0aeff0b
KseniaGlazova <ksenia.glazova@gmail.com>
2020-02-24 18:49:37 +0300

    Попытка задание 10

--- src/MassiveSecond.java
+++ src/MassiveSecond.java
@@ -5,21 +5,19 @@ public class MassiveSecond {
     //private static int i;
 
     public static void main(String[] args) throws Exception{
-                Scanner sc = new Scanner(System.in);
-                int matrix [][] = new int[3][3];
-        System.out.println("Введи числа для матрицы: ");
-
-                for (int i = 0; i < 3; i++){
-                    //char[] in = sc.nextLine().toCharArray();
-                    for (int j = 0; j < 3; j++){
-                        matrix[i][j] = sc.nextInt();
-                        //matrix[i][j] = in[j];
-                        System.out.println(matrix[i][j] + " ");
-                    }
-                    System.out.println();
-                }
-
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введи количество строк в массиве: ");
+        int stroka = sc.nextInt();
+        System.out.println("Введи количество столбцов в массиве: ");
+        int stolbec = sc.nextInt();
+        int [][] matrix = new int[stroka][stolbec];
+        //System.out.println("Введи числа для таблицы: ");
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[i].length; j++){
+                System.out.print("Введите элемент matrix[" + i + "][" + j + "]:");
+                matrix[i][j] = sc.nextInt();
+            }
         }
     }
+}
 
