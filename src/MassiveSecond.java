a16f7d62143629ee481baac03a4b7d38a55dd9a9
KseniaGlazova <ksenia.glazova@gmail.com>
2020-02-24 19:07:12 +0300

    Попытка задание 10/1

--- src/MassiveSecond.java
+++ src/MassiveSecond.java
@@ -11,11 +11,17 @@ public class MassiveSecond {
         System.out.println("Введи количество столбцов в массиве: ");
         int stolbec = sc.nextInt();
         int [][] matrix = new int[stroka][stolbec];
-        //System.out.println("Введи числа для таблицы: ");
         for (int i = 0; i < matrix.length; i++){
             for (int j = 0; j < matrix[i].length; j++){
                 System.out.print("Введите элемент matrix[" + i + "][" + j + "]:");
                 matrix[i][j] = sc.nextInt();
+            }
+        }
+        int x;
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[0].length; j++){
+                j = j * 3;
+                System.out.println(matrix[0][j] + " ");
             }
         }
     }
