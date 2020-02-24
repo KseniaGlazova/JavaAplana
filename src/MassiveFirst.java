678434a4cfdf4cc031719edfd95a95cd54fdc77d
KseniaGlazova <ksenia.glazova@gmail.com>
2020-02-24 14:37:49 +0300

    Исправленное задание 9

--- src/MassiveFirst.java
+++ src/MassiveFirst.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class MassiveFirst {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] numbers = new int[5];
-        int i = 0;
-
-        for (int element : numbers){
+        System.out.println("Введи размер массива: ");
+        int size = scanner.nextInt();
+        int[] numbers = new int[size];
+        System.out.println("Введи числа массива: ");
+        for (int i = 0; i < size; i++){
             numbers[i] = scanner.nextInt();
-            i++;
         }
 
         for (int x : numbers){
