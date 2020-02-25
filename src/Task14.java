e0281000f0dd92ab904d14a5a69492539c4cdde3
KseniaGlazova <ksenia.glazova@gmail.com>
2020-02-25 09:24:45 +0300

    Задание 14

---
+++ src/Task14.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task14 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введи число: ");
+        String s = sc.nextLine();
+        int x = Integer.parseInt(s);
+        double y = x;
+        System.out.println(s);
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
