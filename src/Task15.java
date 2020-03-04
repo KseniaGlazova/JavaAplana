ff033a3c126223804e09dab9f483232e4021d003
KseniaGlazova <ksenia.glazova@gmail.com>
2020-03-04 15:53:11 +0300

    Задание 15: поправила пробелы, сделала задание внутри одного класса, добавила ввод данных для массива с клавиатуры (работа по замечаниям)

--- src/Task15.java
+++ src/Task15.java
@@ -1,25 +1,27 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Task15 {
     public static void main(String[] args) {
-        BubbleSort bubbleSort = new BubbleSort();
-        int[] a = {5, 6, 2, 39, 1, 0, 7, 3, 9, 18};
-        bubbleSort.sort(a);
-    }
-}
-
-    class BubbleSort {
-        public void sort (int[] arr) {
-            for (int i = arr.length-1; i >0; i--) {
-                for (int j = 0; j < i; j++) {
-                    if (arr[j] > arr[j+1]){
-                        int temp = arr[j];
-                        arr[j] = arr[j+1];
-                        arr[j+1] = temp;
-                    }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введи размер массива: ");
+        int size = sc.nextInt();
+        int[] a = new int[size];
+        System.out.println("Наполни массив числами: ");
+        for (int i = 0; i < size; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = a.length-1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (a[j] > a[j + 1]){
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
                 }
             }
-            System.out.println(Arrays.toString(arr));
         }
+        System.out.print("Отсортированный массив: ");
+        System.out.println(Arrays.toString(a));
+        
     }
-
+}
