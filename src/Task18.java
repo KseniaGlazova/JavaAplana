8717260e1f8725e8051cf98ddef5d6f70e565ead
KseniaGlazova <ksenia.glazova@gmail.com>
2020-03-16 19:07:43 +0300

    Задание 18

--- src/Task18.java
+++ src/Task18.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Task18 {
     public static void main(String[] args) {
@@ -14,7 +11,38 @@ public class Task18 {
                 System.out.println(line);
             }
         }catch (IOException exc){
-            System.out.println("Вот тут у тебя ошибка-ввода вывода: " + exc + "Проверь, где у тебя лежит файл, например");
+            System.out.println("Вот тут у тебя ошибка-ввода вывода: " + exc + "Проверь, где у тебя лежит файл или проверь название, например");
         }
+
+        try{
+            File myFile = new File("task18");
+            FileReader fileReader = new FileReader(myFile);
+            LineNumberReader lineNumberReader = new LineNumberReader(fileReader);
+            int lineNumber = 0;
+            while (lineNumberReader.readLine() != null){
+                lineNumber++;
+            }
+            System.out.println("\n\r" +
+                    "Количество строк в файле: " + lineNumber);
+            lineNumberReader.close();
+
+            String s;
+            BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+            System.out.println("После 7-ой строчки запись в файл производиться не будет");
+            FileWriter fw = new FileWriter("task18");
+            int k = 0;
+            System.out.println("Введи свой текст, где пробел означает переход на новую строку: ");
+            s = br.readLine();
+            StringBuilder builder = new StringBuilder();
+            for(String val : s.split(" ")){
+                builder.append(val).append("\n");
+
+            }
+            fw.write(builder.toString());
+            fw.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
