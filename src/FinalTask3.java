c2408e037674b390c87926a7b4169f476492b234
KseniaGlazova <ksenia.glazova@gmail.com>
2020-03-18 18:05:58 +0300

    финальное задание 3 черновик

---
+++ src/FinalTask3.java
@@ -0,0 +1,15 @@
+public class FinalTask3 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println(convertEurToUsd(70,31.5));
+        System.out.println(convertEurToUsd(40,20.6));
+
+    }
+
+    public static double convertEurToUsd(int eur, double course) {
+        //напишите тут ваш код
+        //double dollarUSA;
+        return eur * course;
+
+    }
+}
